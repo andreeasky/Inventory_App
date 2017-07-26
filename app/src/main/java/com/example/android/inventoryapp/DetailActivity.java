0e80552a5413fc60a3e756d833c9b8bb0a815044
Andreea Manea <andreeasky@yahoo.com>
2017-07-26 06:10:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/inventoryapp/DetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.inventoryapp;
+
+import android.app.LoaderManager;
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Allows user to create a new product or edit an existing one.
+ */
+public class DetailActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+}
+
+
---
+++ app/src/main/java/com/example/android/inventoryapp/ProductCursorAdapter.java
@@ -0,0 +1,5 @@
+package com.example.android.inventoryapp;
+
+
+public class ProductCursorAdapter {
+}
